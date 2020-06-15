70c571ed145291200afb547577b029c539308e62
hamza286 <64368418+hamza286@users.noreply.github.com>
2020-06-15 22:51:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -524,3 +524,205 @@
 // } else if (input == time && input >= 2100 || input <= 2359) {
 //     alert("Good night!")
 // }
+// Assignment # 14 TO 16 TASK 1
+
+//var names = ["Name0", "Name1", "Name2"]
+
+// Assignment # 14 TO 16 Task 2
+
+// var names = [];
+
+// names[0] = "";
+// names[1] = "";
+// names[2] = "";
+
+// Assignment # 14 TO 16 TASK 3
+
+
+// var a = ["Hamza", "Osama", "Mansoor"]
+// console.log(a[0], a[1])
+
+// Assignment # 14 TO 16 Task 4
+
+// var a=[1,2,3]
+// console.log(a[0],a[1])
+
+
+// Assignment # 14 TO 16 Task 5
+// var a = [(2 * 3) + 2, (2 / 2) + 1, (4 * 2) - 3]
+
+// Assignment # 14 TO 16 Task 6
+// var a = ["Hamza04", "Hamza02"]
+
+
+// Assignment # 14 TO 16 Task 7
+
+// var b = "<h4>QUALIFICATIONS</h4>"
+// document.write(b)
+// var a = ["SSC<br>", "HSC<br>", "BSC<br>", "BS<br>", "BCOM<br>", "MS<br>", "M.Phil<br>", "PhD<br>"]
+// document.write(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
+
+// Assignment # 14 TO 16 Task 8
+
+// var names = ["Hamza", "Osama", "Mansoor"]
+// var scores = [400, 470, 370]
+// var per = ["Percentage:"]
+// document.write("Score of ", names[0], " is ", scores[0], ".", per[0], scores[0] / 500 * 100, "%<br>",
+//     "Score of ", names[1], " is ", scores[1], ".", per[0], scores[1] / 500 * 100, "%<br>", "Score of ", names[2], " is ", scores[2], ".", per[0], scores[2] / 500 * 100, "%")
+
+// Assignment # 14 TO 16 Task 9
+
+// var color = ["Red ", "Blue ", "Orange ", "Green ", "Purple"]
+// document.write(color + "<br>")
+// // // a)
+// var a = prompt("Add color in  beginning:")
+// color.unshift(a)
+// document.write(color + "<br>")
+// // b)
+// var b = prompt("Add color in end:")
+// color.push(b)
+// document.write(color + "<br>")
+// // // c)
+// color.unshift("Mehroon", "White")
+// document.write(color + "<br>")
+// // // d)
+// color.shift()
+// document.write(color + "<br>")
+// // // e)
+// color.pop()
+// document.write(color + "<br>")
+// // // f)
+// var c = +prompt("Enter color index to add:")
+// var d = prompt("Enter color to add:")
+// color.splice(c, 0, d)
+// document.write(color + "<br>")
+// // g)
+// var e = +prompt("Enter color index to delete:")
+// var f = +prompt("Enter how many colors you want to delete:")
+// color.splice(e, f)
+// document.write(color)
+
+// var d = prompt("Enter color name")
+// color.splice(c, 4, d)
+// document.write(color)
+
+// Assignment # 14 TO 16 Task 10
+
+// var scores = [320, 220, 420, 520]
+// document.write("Scores of Students:", scores + "<br>")
+// scores.sort()
+// document.write("Ordered Scores of Students:", scores)
+
+// Assignment # 14 TO 16 Task 10 Task 11
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
+// document.write("Cities List:<br>", cities + "<br>")
+// var selectedcities = []
+// cities.splice(0, 2)
+// cities.pop()
+// document.write("Selected cities List:<br>", cities)
+
+// Assignment # 14 TO 16 Task 10 Task 12
+// var arr = ["This", "is", "my", "cat"];
+// document.write(arr + "<br>")
+// document.write(arr.join(" "))
+
+// Assignment # 14 TO 16 Task 10 Task 13
+// var a = ["keyboard", "mouse", "printer", "monitor"]
+// var head = "Devices:"
+// document.write(head + "<br>" + a + "<br>")
+// for (let i = 0; i < a.length; i++) {
+//     document.write("Out:<br>" + a[i] + "<br>")
+// }
+// Assignment # 14 TO 16 Task 14
+
+// var a = ["keyboard", "mouse", "printer", "monitor"]
+// var head = "Devices:"
+// document.write(head + "<br>" + a + "<br>")
+// for (i = a.length - 1; i >= 0; i--) {
+//     document.write("Out:<br>" + a[i] + "<br>")
+// }
+// Assignment # 14 TO 16 Task 15
+// var a = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", " Haier"]
+// document.write("<select>")
+// for (var i = 0; i < a.length; i++) {
+//     document.write("<option>" + a[i] + "</option>")
+// }
+// document.write("</select>")
+
+// Assignment # 16 TO 20  Task 1
+
+// var a = [
+//     [1, 2, 3, 4]
+// ]
+
+// Assignment # 16 TO 20 Task 2
+// var a = [
+//     [0, 1, 2, 3, "<br>"],
+//     [1, 0, 1, 2, "<br>"],
+//     [2, 1, 0, 1, "<br>"]
+// ]
+// for (let i = 0; i < a.length; i++) {
+//     for (let k = 0; k < a[i].length; k++) {
+//         document.write(a[i][k])
+//     }
+// }
+
+// Assignment # 16 TO 20 Task 3
+// var a = 10;
+// for (let i = 1; i <= a; i++) {
+//     document.write(i + "<br>")
+// }
+//  Assignment # 16 TO 20 Task 4
+
+// var a = +prompt("Enter a number to show its multiplication table")
+// var b = +prompt("Enter length of multiplication table")
+// for (let i = 1; i <= b; i++) {
+//     document.write(a + "x" + i + "=", a * i + "<br>")
+// }
+// Assignment # 16 TO 20 Task 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for (let k = 0; k < fruits.length; k++) {
+//     document.write(fruits[k] + "<br>")
+// }
+// for (let i = 0; i < fruits.length; i++) {
+//     document.write("Element at index " + i + " is " + fruits[i] + "<br>")
+// }
+
+// Assignment # 16 TO 20 Task 6
+
+// var head = ("<h3>Counting:<br> </h3>")
+// document.write(head)
+// var a = 15;
+// for (var i = 1; i <= a; i++) {
+//     document.write(i, ",")
+// }
+// var head = ("<h3>Reverse Counting:<br> </h3>")
+// document.write(head)
+// var a = 1;
+// for (var i = 10; i >= a; i--) {
+//     document.write(i, ",")
+// }
+// var head = ("<h3>Even:<br> </h3>")
+// document.write(head)
+// var a = 20;
+// for (var i = 0; i <= a; ++i) {
+//     if (i % 2 === 0) {
+//         document.write(i + ",")
+//     }
+// }
+// var head = ("<h3>Odd:<br> </h3>")
+// document.write(head)
+// var a = 20;
+// for (var i = 1; i <= a; ++i) {
+//     if (i % 2 === 1) {
+//         document.write(i + ",")
+//     }
+// }
+// var head = ("<h3>Odd:<br> </h3>")
+// document.write(head)
+// var a = 20;
+// for (var i = 2; i <= a; ++i) {
+//     if (i % 2 === 0) {
+//         document.write(i + "k,")
+//     }
+// }
